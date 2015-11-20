51324f0f2567c3aa6e0af9569da26bd5b5598a9e
RockyFung <315273933@qq.com>
2015-11-20 22:33:04 +0800

    APNSTest

--- APNSTest/AppDelegate.m
+++ APNSTest/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+#import "XGPush.h"
+#import "XGSetting.h"
+
 
 @interface AppDelegate ()
 
@@ -15,10 +18,56 @@
 @implementation AppDelegate
 
 
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    // 初始化信鸽 到信鸽官网申请APPKEY
+    [XGPush startApp:2200163966 appKey:@"I2BU9I1QC79P"];
+    
+    
+
+    // 获取系统当前版本，如果大于8.0就使用最新的创建方法，如果小于8.0就使用8.0已经弃用的方法
+    if ([UIDevice currentDevice].systemVersion.floatValue > 8.0) {
+        // 注册推送
+        UIUserNotificationSettings *set = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound categories:nil];
+        [application registerUserNotificationSettings:set];
+        
+        // 开启推送
+        [application registerForRemoteNotifications];
+        
+    }else{
+        [application registerForRemoteNotificationTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeBadge |UIUserNotificationTypeSound];
+    }
+    
+    
+    
+    
     return YES;
 }
+
+// 推送成功后返回的token,我们使用信鸽进行解析、
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // 获取token的字符串
+    NSString *token = [XGPush registerDevice:deviceToken];
+    NSLog(@"%@",token);
+}
+
+
+// 点击应用后获取推送的信息
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+   
+    // 角标滞空
+    application.applicationIconBadgeNumber = 0;
+     NSLog(@"%@",userInfo);
+}
+
+
+
+
+
+
 
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
